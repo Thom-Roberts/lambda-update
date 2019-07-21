9e5da3d07579c038449d50b1d0d8d30ba14188c3
= <robertsorange@gmail.com>
2019-07-20 21:49:08 -0600

    Added interfaces

---
+++ interfaces.d.ts
@@ -0,0 +1,33 @@
+export interface Member {
+	membershipId: string;
+	membershipType: number;
+	displayName: string;
+}
+
+export interface Stats {
+	// Response.mergedAllCharacters.results
+	pve?: { //allPvE.allTime
+		activitiesCleared: number; //activitiesCleared.basic.value
+		assists: number; //assists.basic.value
+		kills: number; //kills.basic.value
+		timePlayed: string; //secondsPlayed.basic.displayValue
+		timePlayedNumber: number; // secondsPlayed.basic.value
+		deaths: number; //deaths.basic.value
+		kdRatio: string; //killsDeathsRatio.basic.displayValue
+		publicEventsCompleted: number; //publicEventsCompleted.basic.value
+	};
+	pvp?: { //allPvP.allTime
+		activitiesPlayed: number; //activitesEntered.basic.value
+		activitiesWon: number; //activitesWon.basic.value
+		assists: number; //assists.basic.value
+		kills: number; //kills.basic.value
+		timePlayed: string; //secondsPlayed.basic.displayValue
+		deaths: number; //deaths.basic.value
+		bestSingleGameKils: number; //bestSingleGameKills.basic.value
+		opponentsDefeated: number; //opponentsDefeated.basic.value
+		efficiency: string; //efficiency.basic.displayValue
+		kdRatio: string; //killsDeathsRatio.basic.displayValue
+		winLossRatio: string; //winLossRatio.basic.displayValue
+		longestKillSpree: number; //longestKillSpree.basic.value
+	};
+}
